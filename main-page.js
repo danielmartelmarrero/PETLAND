de904ec234d9631ff240d8140518faf9d7014d7d
Daniel Martel <danielmartelmarrero@gmail.com>
2024-07-04 18:06:14 +0100

    change script file 2

--- main-page.js
+++ main-page.js
@@ -5,7 +5,7 @@ function saveInfo(title, image, price, info){
     localStorage.setItem('price',price)
     localStorage.setItem('info', info)
 
-    window.location.href="../Listing-page/product-info.html"
+    window.location.href="./Listing-page/product-info.html"
     //Guardamos los parametros pasados en la función en el local storage
     //Guardamos la información del producto en el local storage cada vez que hacemos click.
 }
